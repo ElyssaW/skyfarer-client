5bd2e0b8ec5f729fcb8e968629108156e4293047
ElyssaW <ejwinch1@gmail.com>
2021-03-01 21:32:56 -0600

    Socket connected

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import io from 'socket.io'
+
+const REACT_APP_SERVER_URL = process.env.REACT_APP_SERVER_URL
 
 function App() {
+
+  let socket = io({
+    auth: {
+      name: "Elyssa"
+    }
+  })
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      Running...
     </div>
-  );
+  )
 }
 
 export default App;
